0dae4e55ee5ce8756e77e7bfd2593b56e03f32c5
nc-niamh-smith <niamh.smith@northcoders.com>
2024-07-26 14:49:23 +0100

    just everything

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div id="bg-img">
+    <section>
+    <h1 id="header">Sumo Wrestling</h1>
+    <nav id="navbar">
+      <a className="out-link" href="https://www3.nhk.or.jp/nhkworld/en/tv/sumo/">Nippon Hoso Kyokai</a>
+    
+      <a className="out-link" href="https://www.japanistry.com/sumo/">History of Sumo</a>
+
+      <a className="out-link" href="https://sumo.pia.jp/en/">Buy Tickets</a>
+    </nav>
+    </section>
+    </div>
+  )
+}
+
+export default App
